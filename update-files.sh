4c02cc500b9f826d5e0cf61b5576ea7e4e88cf47
Fen Nicolas violant <fnicolas@c1r4s5.42madrid.com>
2022-02-05 18:37:49 +0100

    key remapping (caps to ctrl not working yet)

--- update-files.sh
+++ update-files.sh
@@ -1,5 +1,6 @@
 vim=.vimrc
 zsh=.zshrc
+keys=keys-remapping.plist
 
-rm -f $vim; rm -f $zsh
-cp ~/$vim $vim; cp ~/$zsh $zsh
+rm -f $vim; rm -f $zsh; rm -f $keys
+cp ~/$vim $vim; cp ~/$zsh $zsh; cp ~/Library/LaunchAgents/$keys $keys

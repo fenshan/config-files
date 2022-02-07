b8610b56ad7da634db9edf3815b524b616c1ca4c
Fen Nicolás <nicolasviolantfen@gmail.com>
2022-02-07 21:58:18 +0100

    Update local-to-git.sh

--- local-to-git.sh
+++ local-to-git.sh
@@ -4,3 +4,5 @@ keys=keys-remapping.plist
 
 rm -f $vim; rm -f $zsh; rm -f $keys
 cp ~/$vim $vim; cp ~/$zsh $zsh; cp ~/Library/LaunchAgents/$keys $keys
+
+echo "repo updated with your local config files!"
